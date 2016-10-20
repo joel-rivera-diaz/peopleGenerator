c632e1aa03e7fdf6fae7874a90149b19b1f3d8ba
Joel Rivera <tilyxxx@gmail.com>
2016-10-19 22:20:43 -0400

    small details in class 'Person'

--- js/code_ts/Person.ts
+++ js/code_ts/Person.ts
@@ -2,9 +2,14 @@
  * Person
  */
 class Person {
-	private el:any;
+	private _name:string;
+	private _sex:string;
+	private _sexuality:string;
+	private _age:number;
+	private _deathDate:string;
+	private _occupation:string;
 	
 	constructor(  ) {
-		this.el = $('<div></div>')
+		
 	}
 }
