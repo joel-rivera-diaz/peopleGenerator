cb6b80b237c04554313d16d4d21cb32982317ff6
Joel Rivera <Joel Rivera>
2016-10-21 12:41:59 -0400

    css for displaying the generated people

--- js/code_ts/main.ts
+++ js/code_ts/main.ts
@@ -1,21 +1,29 @@
 import { Person } from './Person';
 
 
-let person = new Person();
-let personData = person.getAllData();
 
-let stringToAppend:string = `
-<div>
-${personData.idSquare}
-${personData.id}
-${personData.name}
-${personData.age}
-${personData.sex}
-${personData.occupation}
-${personData.sexuality}
-${personData.deathDate}
-</div>
-`;
 
-$('div#global_container').append( stringToAppend );
+_(20).times( createAndPrintPerson );
 
+
+
+
+function createAndPrintPerson():void{
+	let person = new Person();
+	let personData = person.getAllData();
+
+	let stringToAppend:string = `
+	<li class="person">
+	<div class="id_square">${personData.idSquare.html()}</div>
+	<span class="id">#${personData.id}</span>
+	<span class="name">${personData.name}</span>
+	<span class="age">${personData.age}</span>
+	<span class="sex">${personData.sex}</span>
+	<span class="occupation">${personData.occupation}</span>
+	<span class="sexuality">${personData.sexuality}</span>
+	<span class="deathDate">${personData.deathDate.dateString()}</span>
+	</li>
+	`;
+
+	$('div#global_container ul.person_list').append( stringToAppend );
+}
